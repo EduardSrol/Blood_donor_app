211ebbfb0b75261dea4ecfc5099461bb72695f00
Denis Valko <469026@muni.cz>
2019-10-01 19:11:55 +0200

    added MVC

---
+++ BAD/BAD.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BAD.Web.MvcApplication" Language="C#" %>
--- BAD/BAD/DAL/UnitOfWork.cs
+++ BAD/BAD/DAL/UnitOfWork.cs
@@ -1,4 +1,5 @@
 ﻿using BAD.Data;
+using BAD.Model;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
