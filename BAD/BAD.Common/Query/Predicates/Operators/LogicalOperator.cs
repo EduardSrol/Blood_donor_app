09bd152a80adf1f158f6c50f5e6c13c6aab9f566
Ján Dovjak <469235@muni.cz>
2019-10-15 18:24:08 +0200

    Added Predicates and Operators

---
+++ BAD/BAD.Common/Query/Predicates/Operators/LogicalOperator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BAD.Infrastructure.Query.Predicates.Operators
+{
+    public enum LogicalOperator
+    {
+        AND, OR
+    }
+}
---
+++ BAD/BAD.Common/Query/Predicates/Operators/ValueComparingOperator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BAD.Infrastructure.Query.Predicates.Operators
+{
+    public enum ValueComparingOperator
+    {
+        None,
+        GreaterThan,
+        GreaterThanOrEqual,
+        Equal,
+        NotEqual,
+        LessThan,
+        LessThanOrEqual,
+        StringContains
+    }
+}
