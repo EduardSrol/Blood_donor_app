85799ed6e5f5c64edaa4618ec696733dc15ce46a
Ján Dovjak <469235@muni.cz>
2019-12-07 20:29:25 +0100

    Opening Hours added

--- BAD/BloodDonorApp.DAL.EF/Models/Common/Institution.cs
+++ BAD/BloodDonorApp.DAL.EF/Models/Common/Institution.cs
@@ -4,13 +4,35 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BloodDonorApp.DAL.EF.Models.Common
 {
+
+    public class Time {
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+
+        public Time(int Hours, int Minutes) {
+            this.Hours = Hours;
+            this.Minutes = Minutes;
+       }
+    }
+    public class Day {
+        public Time OpeningTime { get; set; }
+        public Time ClosingTime { get; set; }
+
+        public Day(Time openingTime, Time closingTime) {
+            this.OpeningTime = openingTime;
+            this.ClosingTime = closingTime;
+        }
+    }
+
     public abstract class Institution : Record
     {
-
+        [Required]
         public string City { get; set; }
-
+        [Required]
         public string Street { get; set; }
-
+        [Required]
         public string Name { get; set; }
+
+        public Day[] OpeningHours { get; set; } = new Day[7];
     }
 }
