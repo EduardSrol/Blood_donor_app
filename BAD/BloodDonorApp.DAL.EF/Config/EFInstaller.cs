aed7da20d1fa5a209d810fe5f94c5ef9b64ed7c6
Ján Dovjak <469235@muni.cz>
2019-12-09 16:56:59 +0100

    Published and connectionString

--- BAD/BloodDonorApp.DAL.EF/Config/EFInstaller.cs
+++ BAD/BloodDonorApp.DAL.EF/Config/EFInstaller.cs
@@ -18,7 +18,7 @@ namespace BloodDonorApp.DAL.EF.Config
 {
     public class EFInstaller : IWindsorInstaller
     {
-        internal const string ConnectionString = "Data source=(localdb)\\mssqllocaldb;Database=BLOODDONORAPP-DB;Trusted_Connection=True;MultipleActiveResultSets=true";
+        internal const string ConnectionString = "Data source=dblood.database.windows.net;Database=BLOODDONORAPP-DB;User ID=dBloodAdmin;Password=dBlood2019;Trusted_Connection=False;Encrypt=True;MultipleActiveResultSets=true";
 
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
--- BAD/BloodDonorApp.PL/App_Start/BundleConfig.cs
+++ BAD/BloodDonorApp.PL/App_Start/BundleConfig.cs
@@ -8,6 +8,10 @@ namespace BloodDonorApp.PL
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            BundleTable.EnableOptimizations = false;
+
+
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -22,9 +26,47 @@ namespace BloodDonorApp.PL
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                      "~/js/vendor/modernizr-3.5.0.min.js",
+                      "~/js/vendor/jquery-1.12.4.min.js",
+                      "~/js/popper.min.js",
+                      "~/js/bootstrap.min.js",
+                      "~/js/owl.carousel.min.js",
+                      "~/js/isotope.pkgd.min.js",
+                      "~/js/ajax-form.js",
+                      "~/js/waypoints.min.js",
+                      "~/js/jquery.counterup.min.js",
+                      "~/js/imagesloaded.pkgd.min.js",
+                      "~/js/scrollIt.js",
+                      "~/js/jquery.scrollUp.min.js",
+                      "~/js/wow.min.js",
+                      "~/js/nice-select.min.js",
+                      "~/js/jquery.slicknav.min.js",
+                      "~/js/jquery.magnific-popup.min.js",
+                      "~/js/plugins.js",
+                      "~/js/gijgo.min.js",
+                      "~/js/contact.js",
+                      "~/js/jquery.ajaxchimp.min.js",
+                      "~/js/jquery.form.js",
+                      "~/js/jquery.validate.min.js",
+                      "~/js/mail-script.js",
+                      "~/js/main.js"));
+
+            bundles.Add(new StyleBundle("~/css/font-awesome.min.css").Include(
+                                      "~/Content/font-awesome.min.css"
+                ));
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/style.css",
+                      "~/Content/Site.css",
+                      "~/Content/animate.css",
+                      "~/Content/magnific-popup.css",
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/themify-icons.css",
+                      "~/Content/nice-select.css",
+                      "~/Content/flaticon.css",
+                      "~/Content/gijgo.css",
+                      "~/Content/slicknav.css"));
         }
     }
 }
